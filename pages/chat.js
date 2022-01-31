16f0d075d22c9d095feca4ae5d9478a6abe4ed6b
fcorrea <fernanda.correa@pdmfc.com>
2022-01-31 10:35:45 +0000

    refactoring

--- pages/chat.js
+++ pages/chat.js
@@ -1,63 +1,33 @@
-import { Box, Text, TextField, Image, Button } from '@skynexui/components';
-import React from 'react';
-import appConfig from '../config.json';
-import { useRouter } from 'next/router'
-import { createClient } from '@supabase/supabase-js'
-import { ButtonSendSticker } from '../src/components/ButtonSendSticker';
+import { Box, Text, TextField, Image, Button } from "@skynexui/components";
+import React from "react";
+import appConfig from "../config.json";
+import { useRouter } from "next/router";
+import { createClient } from "@supabase/supabase-js";
+import { ButtonSendSticker } from "../src/components/ButtonSendSticker";
 
 // Como fazer AJAX: https://medium.com/@omariosouto/entendendo-como-fazer-ajax-com-a-fetchapi-977ff20da3c6
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MzQ5NjU5OSwiZXhwIjoxOTU5MDcyNTk5fQ.d-vDumq7YjoQnxmepfYJ13J5s_xlQq3L--dZcjzd4M4';
-const SUPABASE_URL = 'https://lhupywrddluubagaukss.supabase.co';
+const SUPABASE_ANON_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MzQ5NjU5OSwiZXhwIjoxOTU5MDcyNTk5fQ.d-vDumq7YjoQnxmepfYJ13J5s_xlQq3L--dZcjzd4M4";
+const SUPABASE_URL = "https://lhupywrddluubagaukss.supabase.co";
 const supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-
-function escutaMensagensEmTempoReal(adicionaMensagem) {
-    return supabaseClient
-      .from('mensagens')
-      .on('INSERT', (respostaLive) => {
-        adicionaMensagem(respostaLive.new);
-      })
-      .subscribe();
-  }
-
 
 export default function ChatPage() {
   const roteamento = useRouter();
   const usuarioLogado = roteamento.query.username;
-  const [mensagem, setMensagem] = React.useState('');
+  const [mensagem, setMensagem] = React.useState("");
   const [listaDeMensagens, setListaDeMensagens] = React.useState([]);
+  const [isLoaded, setIsLoaded] = React.useState(false);
 
   React.useEffect(() => {
     supabaseClient
-      .from('mensagens')
-      .select('*')
-      .order('id', { ascending: false })
+      .from("mensagens")
+      .select("*")
+      .order("id", { ascending: false })
       .then(({ data }) => {
-        // console.log('Dados da consulta:', data);
+        //console.log('Dados da consulta:', data);
         setListaDeMensagens(data);
+        setIsLoaded(true);
       });
-
-    const subscription = escutaMensagensEmTempoReal((novaMensagem) => {
-      console.log('Nova mensagem:', novaMensagem);
-      console.log('listaDeMensagens:', listaDeMensagens);
-      // Quero reusar um valor de referencia (objeto/array) 
-      // Passar uma função pro setState
-
-      // setListaDeMensagens([
-      //     novaMensagem,
-      //     ...listaDeMensagens
-      // ])
-      setListaDeMensagens((valorAtualDaLista) => {
-        console.log('valorAtualDaLista:', valorAtualDaLista);
-        return [
-          novaMensagem,
-          ...valorAtualDaLista,
-        ]
-      });
-    });
-
-    return () => {
-      subscription.unsubscribe();
-    }
   }, []);
 
   function handleNovaMensagem(novaMensagem) {
@@ -68,53 +38,59 @@ export default function ChatPage() {
     };
 
     supabaseClient
-      .from('mensagens')
+      .from("mensagens")
       .insert([
         // Tem que ser um objeto com os MESMOS CAMPOS que você escreveu no supabase
-        mensagem
+        mensagem,
       ])
       .then(({ data }) => {
-        console.log('Criando mensagem: ', data);
+        console.log("Criando mensagem: ", data);
+        setListaDeMensagens([data[0], ...listaDeMensagens]);
       });
 
-    setMensagem('');
+    setMensagem("");
   }
 
   return (
     <Box
       styleSheet={{
-        display: 'flex', alignItems: 'center', justifyContent: 'center',
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
         backgroundColor: appConfig.theme.colors.primary["100"],
         backgroundImage: `url(https://www.nawpic.com/media/2020/demon-slayer-desktop-nawpic-2.jpg)`,
-        backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
-        color: appConfig.theme.colors.neutrals['000']
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        backgroundBlendMode: "multiply",
+        color: appConfig.theme.colors.neutrals["000"],
       }}
     >
       <Box
         styleSheet={{
-          display: 'flex',
-          flexDirection: 'column',
+          display: "flex",
+          flexDirection: "column",
           flex: 1,
-          boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
-          borderRadius: '5px',
+          boxShadow: "0 2px 10px 0 rgb(0 0 0 / 20%)",
+          borderRadius: "5px",
           backgroundColor: appConfig.theme.colors.neutrals[700],
-          height: '100%',
-          maxWidth: '95%',
-          maxHeight: '95vh',
-          padding: '32px',
+          height: "100%",
+          maxWidth: "95%",
+          maxHeight: "95vh",
+          padding: "32px",
         }}
       >
-        <Header />
+        <Header usuarioLogado={usuarioLogado} />
+        {isLoaded ? "" : <Loading />}
         <Box
           styleSheet={{
-            position: 'relative',
-            display: 'flex',
+            position: "relative",
+            display: "flex",
             flex: 1,
-            height: '80%',
+            height: "80%",
             backgroundColor: appConfig.theme.colors.neutrals[600],
-            flexDirection: 'column',
-            borderRadius: '5px',
-            padding: '16px',
+            flexDirection: "column",
+            borderRadius: "5px",
+            padding: "16px",
           }}
         >
           <MessageList mensagens={listaDeMensagens} />
@@ -128,8 +104,7 @@ export default function ChatPage() {
           <Box
             as="form"
             styleSheet={{
-              display: 'flex',
-
+              display: "flex",
             }}
           >
             <TextField
@@ -139,7 +114,7 @@ export default function ChatPage() {
                 setMensagem(valor);
               }}
               onKeyPress={(event) => {
-                if (event.key === 'Enter') {
+                if (event.key === "Enter") {
                   event.preventDefault();
                   handleNovaMensagem(mensagem);
                 }
@@ -147,17 +122,16 @@ export default function ChatPage() {
               placeholder="Insira sua mensagem aqui..."
               type="textarea"
               styleSheet={{
-                width: '100%',
-                border: '0',
-                resize: 'none',
-                borderRadius: '5px',
-                padding: '6px 8px',
+                width: "100%",
+                border: "0",
+                resize: "none",
+                borderRadius: "5px",
+                padding: "6px 8px",
                 backgroundColor: appConfig.theme.colors.neutrals[800],
-                marginRight: '12px',
+                marginRight: "12px",
                 color: appConfig.theme.colors.neutrals[200],
               }}
             />
-            {/* CallBack */}
             <ButtonSendSticker
               onStickerClick={(sticker) => {
                 // console.log('[USANDO O COMPONENTE] Salva esse sticker no banco', sticker);
@@ -172,8 +146,8 @@ export default function ChatPage() {
                 handleNovaMensagem(mensagem);
               }}
               styleSheet={{
-                backgroundColor: appConfig.theme.colors.neutrals[800],
                 marginLeft: '8px',
+                backgroundColor: appConfig.theme.colors.neutrals[800],
                 hover: {
                   backgroundColor: appConfig.theme.colors.neutrals[200],
                 },
@@ -186,39 +160,66 @@ export default function ChatPage() {
         </Box>
       </Box>
     </Box>
-  )
+  );
 }
 
-function Header() {
+function Header(mensagem) {
   return (
     <>
-      <Box styleSheet={{ width: '100%', marginBottom: '16px', display: 'flex', alignItems: 'center', justifyContent: 'space-between' }} >
-        <Text variant='heading5'>
-          Chat
-        </Text>
+      <Box
+        styleSheet={{
+          width: "100%",
+          marginBottom: "16px",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <Text variant="heading5">Bem vindo(a) {mensagem.usuarioLogado}</Text>
         <Button
-          variant='tertiary'
-          colorVariant='neutral'
-          label='Logout'
+          variant="tertiary"
+          colorVariant="neutral"
+          label="Logout"
           href="/"
         />
       </Box>
     </>
-  )
+  );
+}
+
+function Loading() {
+  return (
+    <>
+      <Image
+        src="https://c.tenor.com/BMrCM1w6qDYAAAAC/demon-slayer.gif"
+        styleSheet={{
+          display: "block",
+          width: "80px",
+          height: "80px",
+          borderRadius: "50%",
+          position: "absolute",
+          left: "50%",
+          top: "50%",
+          marginLeft: "-40px",
+          zIndex: "1",
+        }}
+      />
+    </>
+  );
 }
 
 function MessageList(props) {
-  //console.log(props);
+  console.log("props:", props);
   return (
     <Box
       tag="ul"
       styleSheet={{
-        overflow: 'scroll',
-        display: 'flex',
-        flexDirection: 'column-reverse',
+        overflow: "scroll",
+        display: "flex",
+        flexDirection: "column-reverse",
         flex: 1,
         color: appConfig.theme.colors.neutrals["000"],
-        marginBottom: '16px',
+        marginBottom: "16px",
       }}
     >
       {props.mensagens.map((mensagem) => {
@@ -227,41 +228,39 @@ function MessageList(props) {
             key={mensagem.id}
             tag="li"
             styleSheet={{
-              borderRadius: '5px',
-              padding: '6px',
-              marginBottom: '12px',
+              borderRadius: "5px",
+              padding: "6px",
+              marginBottom: "12px",
               hover: {
                 backgroundColor: appConfig.theme.colors.neutrals[700],
-              }
+              },
             }}
           >
             <Box
               styleSheet={{
-                marginBottom: '8px',
+                marginBottom: "8px",
               }}
             >
               <Image
                 styleSheet={{
-                  width: '20px',
-                  height: '20px',
-                  borderRadius: '50%',
-                  display: 'inline-block',
-                  marginRight: '8px',
+                  width: "20px",
+                  height: "20px",
+                  borderRadius: "50%",
+                  display: "inline-block",
+                  marginRight: "8px",
                 }}
                 src={`https://github.com/${mensagem.de}.png`}
               />
-              <Text tag="strong">
-                {mensagem.de}
-              </Text>
+              <Text tag="strong">{mensagem.de}</Text>
               <Text
                 styleSheet={{
-                  fontSize: '10px',
-                  marginLeft: '8px',
+                  fontSize: "10px",
+                  marginLeft: "8px",
                   color: appConfig.theme.colors.neutrals[300],
                 }}
                 tag="span"
               >
-                {(new Date().toLocaleDateString())}
+                {new Date().toLocaleDateString()}
               </Text>
             </Box>
             {/* mensagem.texto*/}
@@ -283,5 +282,5 @@ function MessageList(props) {
         );
       })}
     </Box>
-  )
+  );
 }
